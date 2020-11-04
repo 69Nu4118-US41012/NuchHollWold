bb3bce76e43c98535a9a8c18bbc31bcc78f73961
Nuch1991 <69138221+Nuch1991@users.noreply.github.com>
2020-11-04 23:41:12 +0700

    Initialize greenkeeper-chai-4.0.0

---
+++ bin/cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+var standardVersion = require('../index')
+var cmdParser = require('../command')
+
+/* istanbul ignore if */
+if (process.version.match(/v(\d+)\./)[1] < 4) {
+  console.error('standard-version: Node v4 or greater is required. `standard-version` did not run.')
+} else {
+  standardVersion(cmdParser.argv, function (err) {
+    if (err) {
+      process.exit(1)
+    }
+  })
+}
